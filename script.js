93ca42bb2cec2de8e7c6c4a20aa6ea21f28231ea
Yana <exvypr296@gmail.com>
2021-06-11 00:16:48 +0300

    add files

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Goodbye, Omegle Terms
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Kill Omegle Terms
+// @author       You
+// @match        *://www.omegle.com
+// @icon         https://www.google.com/s2/favicons?domain=omegle.com
+// @author       Yana
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    termsLevel = 1;
+})();
